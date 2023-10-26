272ba8ac98835a4091dfbbe0b9c25327508d23dd
tewedaj <tewedaj13@gmail.com>
2023-10-26 10:57:05 +0300

    * PWA configuration added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      },
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Westerlies',
+        short_name: 'Westerlies',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './src/assets/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
