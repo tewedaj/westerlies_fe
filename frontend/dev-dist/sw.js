4d8c0e53b0599647a07d8ca2b8eacf498c5194b8
sarawoldeyohannes <sarawoldyohannes@gmail.com>
2023-11-03 11:55:06 +0300

    ..

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.icrna13sts8"
+    "revision": "0.sr0g8vqtbo8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
